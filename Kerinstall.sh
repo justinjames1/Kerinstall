2ce3bb44674681d95a4e344cbd4535a3cb96e064
Justin <justindude057@gmail.com>
2026-05-24 14:51:39 -0400

    Update Kerinstall.sh

--- Kerinstall.sh
+++ Kerinstall.sh
@@ -14,25 +14,25 @@ echo comepleted preping files for compilation.
 nasm -f bin BOOTSECTION.asm -o BOOTSECTION.bin
 cat BOOTSECTION.bin
 cc /root/kernel/Diskmanage.h -o Diskmanage.bin
-cat kernel/Diskmanage.bin
+cat /kernel/Diskmanage.bin
 cc /root/kernel/Diskmanage_code.c -o Diskmanage_code.bin
-cat kernel/Diskmanage_code.bin
+cat /kernel/Diskmanage_code.bin
 cc /root/kernel/Interrupt.h -o Interrupt.bin
-cat kernel/Interrupt.bin
+cat /kernel/Interrupt.bin
 cc /root/kernel/Interrupt_code.c -o Interrupt_code.bin
-cat kernel/Interrupt_code.bin
+cat /kernel/Interrupt_code.bin
 cc /root/kernel/Memcall.h -o Memcall.bin
-cat kernel/Memcall.bin
+cat /kernel/Memcall.bin
 cc /root/kernel/Memcall_code.c -o Memcall_code.bin
-cat kernel/Memcall_code.bin
+cat /kernel/Memcall_code.bin
 cc /root/kernel/Panic.c -o Panic.bin
-cat kernel/Panic.bin
+cat /kernel/Panic.bin
 cc /root/kernel/Process_scheduler.c -o Process_scheduler.bin
-cat kernel/Process_scheduler.bin
+cat /kernel/Process_scheduler.bin
 cc /root/kernel/Krnlcall.c -o Krnlcall.bin
-cat kernel/Krnlcall.bin
+cat /kernel/Krnlcall.bin
 nasm -f bin /root/kernel/VGAcontrol.asm -o VGAcontrol.bin
-cat kernel/VGAcontrol.bin
+cat /kernel/VGAcontrol.bin
 cc /root/Commandprompt.c -o Commandprompt.bin
 cat root/Commandprompt.bin
 mv kernel/* .
